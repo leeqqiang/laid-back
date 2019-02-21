ae6bc5ebce78055704305d5daeedb88bb24928b8
liyuqiang <liyuqiang@yidian-inc.com>
2019-02-21 10:54:40 +0800

    update three sum

--- Sum/three-sum.js
+++ Sum/three-sum.js
@@ -9,8 +9,46 @@
  */
 var threeSum = function(nums) {
   if(nums.length === 0) return []
+  let result = []
 
-  let map = {}
+  nums = nums.sort((a, b) => a - b)
 
+  for(let i = 0, len = nums.length; i < len - 2; i++) {
+    if(nums[i] > 0) return result
+    if(i > 0 && nums[i] === nums[i - 1]) continue
 
+    for(let j = i + 1, k = len - 1; j < k;) {
+      let valI = nums[i]
+      let valJ = nums[j]
+      let valK = nums[k]
+      let sum = valI + valJ + valK
+
+      if(sum === 0) {
+        result.push([valI, valJ, valK])
+        j++
+        k--
+
+        while(j < k && nums[j] === nums[j - 1]) {
+          j++
+        }
+
+        while(j < k && nums[k] === nums[k + 1]) {
+          k--
+        }
+      }
+      else if(sum > 0) {
+        k--
+      }
+      else {
+        j++
+      }
+    }
+  }
+
+  return result
 }
+
+let nums = [-1, 0, 1, 2, -1, -4]
+let x = threeSum(nums)
+
+console.log('x: ', x)
